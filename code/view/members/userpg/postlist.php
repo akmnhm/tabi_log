43d71bfdcbbcd4f89f77c052a50f3432c0ae1b29
Haruka Matsumoto <harukamatsumoto@Harukas-MacBook-Air.local>
2016-01-27 06:41:18 +0900

    postlist for userpage

---
+++ code/view/members/userpg/postlist.php
@@ -0,0 +1,184 @@
+<html>
+  <head>
+    <?php echo Asset::css("smalltable.css"); ?>
+  </head>
+  <body>
+
+    <?php echo $username."->".$pagename; ?><br>
+
+    <table class="topic">
+      <tr><td class="line"></td></tr>
+      <tr><td class="content">青森県</td></tr>
+    </table>
+
+    <table class="minilst"><tr><td>
+	  <!-- 1 post -->
+	  <table class="mini"><tr><td>
+		<table class="minitop" ><tr><th rowspan="4" valign="top">
+		      <table class="icon"><tr><td>
+			    <?php 
+			       $options = array('width' => '50', 'height' => '50');
+			    echo Asset::img("uimg/hiraizumi.jpg", $options); ?>
+		      </td></tr></table>
+		    </th><td><a href="#"><romitt>平泉</romitt></a></td></tr>
+		  <tr><td valign="top"><romitt>自然にふれる</romitt></td></tr>
+		  <tr><td valign="top"><romitt>寺・yamayamayamでっでfrっfrっっfっrfrfでd</romitt></td></tr>
+		  <tr><td valign="top">☆☆☆☆☆</td></tr>
+	    </table></td></tr><tr>
+	      <td class="line"></td></tr><tr>
+	      <td class="context"><omitt>title/title/title/title/title</omitt></td></tr></table>
+	  <!--  ----- -->
+	</td><td>
+	  <!-- 1 post -->
+	  <table class="mini"><tr><td>
+		<table class="minitop" ><tr><th rowspan="4" valign="top">
+		      <table class="icon"><tr><td>
+			    <?php 
+			       $options = array('width' => '50', 'height' => '50');
+			    echo Asset::img("uimg/hiraizumi.jpg", $options); ?>
+		      </td></tr></table>
+		    </th><td><a href="#"><romitt>平泉</romitt></a></td></tr>
+		  <tr><td valign="top"><romitt>自然にふれる</romitt></td></tr>
+		  <tr><td valign="top"><romitt>寺・yamayamayamでっでfrっfrっっfっrfrfでd</romitt></td></tr>
+		  <tr><td valign="top">☆☆☆☆☆</td></tr>
+	    </table></td></tr><tr>
+	      <td class="line"></td></tr><tr>
+	      <td class="context"><omitt>title/title/title/title/title</omitt></td></tr></table>	  <!-- ------ -->
+    </td></tr></table>
+
+    <table class="topic">
+      <tr><td class="line"></td></tr>
+      <tr><td class="content">埼玉県</td></tr>
+    </table>
+
+    <table class="minilst"><tr><td>
+	  <!-- 1 post -->
+	  <table class="mini"><tr><td>
+		<table class="minitop" ><tr><th rowspan="4" valign="top">
+		      <table class="icon"><tr><td>
+			    <?php 
+			       $options = array('width' => '50', 'height' => '50');
+			    echo Asset::img("uimg/hiraizumi.jpg", $options); ?>
+		      </td></tr></table>
+		    </th><td><a href="#"><romitt>平泉</romitt></a></td></tr>
+		  <tr><td valign="top"><romitt>自然にふれる</romitt></td></tr>
+		  <tr><td valign="top"><romitt>寺・yamayamayamでっでfrっfrっっfっrfrfでd</romitt></td></tr>
+		  <tr><td valign="top">☆☆☆☆☆</td></tr>
+	    </table></td></tr><tr>
+	      <td class="line"></td></tr><tr>
+	      <td class="context"><omitt>title/title/title/title/title</omitt></td></tr></table>	  <!-- 1 ----- -->
+	</td><td>
+	  <!-- 1 post -->
+	  <table class="mini"><tr><td>
+		<table class="minitop" ><tr><th rowspan="4" valign="top">
+		      <table class="icon"><tr><td>
+			    <?php 
+			       $options = array('width' => '50', 'height' => '50');
+			    echo Asset::img("uimg/hiraizumi.jpg", $options); ?>
+		      </td></tr></table>
+		    </th><td><a href="#"><romitt>平泉</romitt></a></td></tr>
+		  <tr><td valign="top"><romitt>自然にふれる</romitt></td></tr>
+		  <tr><td valign="top"><romitt>寺・yamayamayamでっでfrっfrっっfっrfrfでd</romitt></td></tr>
+		  <tr><td valign="top">☆☆☆☆☆</td></tr>
+	    </table></td></tr><tr>
+	      <td class="line"></td></tr><tr>
+	      <td class="context"><omitt>title/title/title/title/title</omitt></td></tr></table>
+	  <!-- ------ -->
+      </td></tr><tr><td>
+	  <!-- 1 post -->
+	  <table class="mini"><tr><td>
+		<table class="minitop" ><tr><th rowspan="4" valign="top">
+		      <table class="icon"><tr><td>
+			    <?php 
+			       $options = array('width' => '50', 'height' => '50');
+			    echo Asset::img("uimg/hiraizumi.jpg", $options); ?>
+		      </td></tr></table>
+		    </th><td><a href="#"><romitt>平泉</romitt></a></td></tr>
+		  <tr><td valign="top"><romitt>自然にふれる</romitt></td></tr>
+		  <tr><td valign="top"><romitt>寺・yamayamayamでっでfrっfrっっfっrfrfでd</romitt></td></tr>
+		  <tr><td valign="top">☆☆☆☆☆</td></tr>
+	    </table></td></tr><tr>
+	      <td class="line"></td></tr><tr>
+	      <td class="context"><omitt>title/title/title/title/title</omitt></td></tr></table>
+	  <!-- ----- --->
+	</td><!-- ここからダミー--> <td></td><!-- ダミー終わり--></tr></table> <!-- 奇数の場合はダミーのtdをいれる-->
+
+    <table class="topic">
+      <tr><td class="line"></td></tr>
+      <tr><td class="content">鹿児島県</td></tr>
+    </table>
+
+
+    <table class="minilst"><tr><td>
+	  <!-- 1 post -->
+	  <table class="mini"><tr><td>
+		<table class="minitop" ><tr><th rowspan="4" valign="top">
+		      <table class="icon"><tr><td>
+			    <?php 
+			       $options = array('width' => '50', 'height' => '50');
+			    echo Asset::img("uimg/hiraizumi.jpg", $options); ?>
+		      </td></tr></table>
+		    </th><td><a href="#"><romitt>平泉</romitt></a></td></tr>
+		  <tr><td valign="top"><romitt>自然にふれる</romitt></td></tr>
+		  <tr><td valign="top"><romitt>寺・yamayamayamでっでfrっfrっっfっrfrfでd</romitt></td></tr>
+		  <tr><td valign="top">☆☆☆☆☆</td></tr>
+	    </table></td></tr><tr>
+	      <td class="line"></td></tr><tr>
+	      <td class="context"><omitt>title/title/title/title/title</omitt></td></tr></table>
+	  <!-- 1 ----- -->
+	</td><td>
+	  <!-- 1 post -->
+	  <table class="mini"><tr><td>
+		<table class="minitop" ><tr><th rowspan="4" valign="top">
+		      <table class="icon"><tr><td>
+			    <?php 
+			       $options = array('width' => '50', 'height' => '50');
+			    echo Asset::img("uimg/hiraizumi.jpg", $options); ?>
+		      </td></tr></table>
+		    </th><td><a href="#"><romitt>平泉</romitt></a></td></tr>
+		  <tr><td valign="top"><romitt>自然にふれる</romitt></td></tr>
+		  <tr><td valign="top"><romitt>寺・yamayamayamでっでfrっfrっっfっrfrfでd</romitt></td></tr>
+		  <tr><td valign="top">☆☆☆☆☆</td></tr>
+	    </table></td></tr><tr>
+	      <td class="line"></td></tr><tr>
+	      <td class="context"><omitt>title/title/title/title/title</omitt></td></tr></table>
+	  <!-- ------ -->
+      </td></tr><tr><td>
+	  <!-- 1 post -->
+	  <table class="mini"><tr><td>
+		<table class="minitop" ><tr><th rowspan="4" valign="top">
+		      <table class="icon"><tr><td>
+			    <?php 
+			       $options = array('width' => '50', 'height' => '50');
+			    echo Asset::img("uimg/hiraizumi.jpg", $options); ?>
+		      </td></tr></table>
+		    </th><td><a href="#"><romitt>平泉</romitt></a></td></tr>
+		  <tr><td valign="top"><romitt>自然にふれる</romitt></td></tr>
+		  <tr><td valign="top"><romitt>寺・yamayamayamでっでfrっfrっっfっrfrfでd</romitt></td></tr>
+		  <tr><td valign="top">☆☆☆☆☆</td></tr>
+	    </table></td></tr><tr>
+	      <td class="line"></td></tr><tr>
+	      <td class="context"><omitt>title/title/title/title/title</omitt></td></tr></table>
+	  <!-- 1 ----- -->
+	</td><td>
+	  <!-- 1 post -->
+	  <table class="mini"><tr><td>
+		<table class="minitop" ><tr><th rowspan="4" valign="top">
+		      <table class="icon"><tr><td>
+			    <?php 
+			       $options = array('width' => '50', 'height' => '50');
+			    echo Asset::img("uimg/hiraizumi.jpg", $options); ?>
+		      </td></tr></table>
+		    </th><td><a href="#"><romitt>平泉</romitt></a></td></tr>
+		  <tr><td valign="top"><romitt>自然にふれる</romitt></td></tr>
+		  <tr><td valign="top"><romitt>寺・yamayamayamでっでfrっfrっっfっrfrfでd</romitt></td></tr>
+		  <tr><td valign="top">☆☆☆☆☆</td></tr>
+	    </table></td></tr><tr>
+	      <td class="line"></td></tr><tr>
+	      <td class="context"><omitt>title/title/title/title/title</omitt></td></tr></table>
+
+    </td></tr></table>
+    
+    <br>
+  </body>
+</html>
