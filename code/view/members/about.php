494040f58b5d57cc9129714da474570e613ef767
Haruka Matsumoto <harukamatsumoto@Harukas-MacBook-Air.local>
2016-01-27 15:22:47 +0900

    add page explaining tabi_log

---
+++ code/view/members/about.php
@@ -0,0 +1,44 @@
+<html>	
+  <head>
+    <?php echo Asset::css("table.css"); ?>
+  </head>
+  <body>
+
+    <table class="topic">
+      <tr><td class="line"></td></tr>
+      <tr><td class="content">旅ログとは？</td></tr>
+    </table>
+    <table width="700px"><tr><td colspan="2">
+	  <h2>何もかも嫌になって旅に出たいと思うこと、ありませんか？</h2></td></tr><tr><td>
+	  <?php echo Asset::img("fimg/tabi.png"); ?></td><td valign="top">
+	  <li>代わり映えのない日々に飽きて、毎週課題にうなされ、もう嫌になるとき。</li>
+	  <li>周りにいる人間と波長が合わないとき。</li>
+	  <li>何もかもどうでもよくなって、全てを投げ出したいとき。</li>
+	  <li>何かが虚しいけど、言葉にできなくて苦しいとき。</li>
+	  <br>
+	  旅ログ管理者はそんなあなたに放浪の旅を推奨します！
+	  上で述べたような状況にいる方は「物事に近づきすぎている」&&「視点に偏りのある」人間です。
+	  よくよく思い出して下さい。そんなちっぽけな悩みなどどうでもいいし、
+	  世界はもっと広く、もっともっと素晴らしいものに満ち溢れています！！<br><br>
+	</td>
+      <tr><td colspan="2">
+	  だいたい、知り合いばかりに囲まれて、毎日同じ人と話していては見えるものも見えないのです。
+	  旅に出て、知らない人間に話しかけたり、見知らぬ道を地図なしで(携帯も機内モードにして)歩き、
+	  美味しいものを一人で食べたりするとき、
+	  そこにはいつもとは全く違う「あなた」がいるはずです。
+	  保証します、その「あなた」がきっと悩みを切り開くキーポイントになるはずです！
+	  つまり、旅は最高にして最強の究極の悩み解決方法なのです！！！
+	  そしてそれを全力で支援するのが、日本の旅先・思い出ガイド「旅ログ」です！
+    </td></tr><tr><td colspan="2" align="right">By 旅ログ管理者</td></tr></table>
+    
+    <?php 
+       $options = array('width' => '180',
+    'height' => '135');
+    echo Asset::img("fimg/about_1.jpg", $options).Asset::img("fimg/about_2.jpg", $options);
+    echo Asset::img("fimg/about_5.jpg", $options).Asset::img("fimg/about_6.jpg", $options); ?>
+    
+    <table><tr><td></td>
+    <br><br><br><br><br><br>
+
+  </body>
+</html>
