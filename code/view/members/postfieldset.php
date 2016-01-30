e5093b13ef3dc7e5073f7eb45887136fbcb2f405
Mariko Nakagawa <g1320526@is.ocha.ac.jp>
2016-01-30 18:41:57 +0900

    fieldsetをもちいた投稿画面

---
+++ code/view/members/postfieldset.php
@@ -0,0 +1,19 @@
+<html>
+  <head>  
+    ユーザ登録
+  </head>
+
+  <body>
+    <!-- 
+	 <?php echo Form::open(array('enctype'=>'multipart/form-data', 'method'=>'post')); ?> 
+	 -->
+    <?php echo $form; ?>
+    
+    <!--
+    <?php Form::close(); ?>
+    -->
+        
+  </body>
+
+</html>
+ 
