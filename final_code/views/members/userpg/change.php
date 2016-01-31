a0bc0fc0c2992454023b6750ee1aa7b282ee0fff
Mariko Nakagawa <g1320526@is.ocha.ac.jp>
2016-02-01 04:10:21 +0900

    ユーザ名アイコン変更可

--- final_code/views/members/userpg/change.php
+++ final_code/views/members/userpg/change.php
@@ -12,13 +12,30 @@
 
       <table class="form"><tr><td colspan="2">アカウント情報を変更します。</td></tr><tr>
 	  <td clospan="2" class="line"></td></tr><tr>
-	  <td class="widh">ユーザ名</td><td><?php echo $info['username']; ?></td><tr>
-	  <td>パスワード</td><td>
-	    <table><tr><td>form</td><tr><td>form</td></tr></table> </td><tr>
-	  <td>名前</td><td>form</td><tr>
-	  <td>アイコン</td><td>変更</td></tr><tr>
-          <td colspan="2" align="center">
-	    <a class="btn space" href="#">変更を保存する</a></td></tr></table>
+	  <td class="widh">ユーザ名</td>
+	  <td>
+	    <?php echo Form::open(array('enctype'=>'multipart/form-data', 'method'=>'post')); ?>
+	    <?php if(isset($newname)){$name = $newname;}else{$name = $info['username'];} ?>
+	  <?php echo Form::input('uname', $name, array('size'=>25)); ?> 
+	  </td>
+	  <td><?php echo Form::submit('ch_name', 'ユーザ名を変更する', array('class'=>'btn btn-default')); ?> <td>
+	  <tr>
+	  
+	  <td>アイコン</td>
+	  <td><?php echo Form::file('upload', array('class'=>'span4')); ?></td>
+	  <td>
+	    <?php echo Form::submit('ch_image', 'アイコン画像を変更する', array('class'=>'btn btn-default')); ?> 
+	    <?php echo Form::close(); ?>
+	    
+	  </td></tr>
+	  
+	    
+	  <tr>
+          </tr>
+	</table>
+      <?php if(isset($upload_err)): ?>
+      <font color="#ff0000"><?php echo $upload_err?></font>
+      <?php endif; ?>
       
       <table class="addspace"><tr><td>旅に出ろ！</td></tr></table>
   </body>
