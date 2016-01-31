fcb0bf575624ba6c65b4e0da938e65413134179f
Mariko Nakagawa <g1320526@is.ocha.ac.jp>
2016-01-31 22:32:43 +0900

    postlookuprev.phpにレビューフォーム追加

--- final_code/views/members/postlookuprev.php
+++ final_code/views/members/postlookuprev.php
@@ -38,16 +38,75 @@
 	 </td><tr><td><table class="reviewmain">
 	       <tr><td><table width="100%"><tr><td class="rating">
 			 評価数: <?php echo $review['rrating']; ?></td>
-		       <td class="date"><?php echo $review['rdatetime']; ?></td></tr>
+			     <td class="date"><?php echo date("Y/n/j H:i", $review['rdatetime']); ?></td></tr>
 	       </table></td></tr><tr>
 		 <td class="content"><?php echo $review['comment']; ?></td></tr></table>
 	</td></tr></table>       
        <?php endforeach; ?>  
 
+       <br>
+
+
+
 
        <!--  投稿ふぉーむ　-->
-     <?php if(count($reviews) < 2){
-	echo "<table class=\"addspace\"><tr><td>(- -)</td></tr></table>";} ?>
+       
+       <?php echo Form::open(array('method'=>'post')); ?>
+       <!-- 
+	    タイトルフォームstart
+	 -->
+       <div class="form-group">
+	 <?php echo Form::label('レビュータイトル', 'title'); ?>
+	 <?php if(isset($input_title)){$input_1 = $input_title;}else{$input_1 = '';} ?>
+	 <?php echo Form::input('title', $input_1, array('size'=>25)); ?> 
+       </div>
+       <!-- 
+	    タイトルのエラー
+	 -->
+       <?php if(isset($val) && $val->error('title')): ?>
+       <p class="alert alert-warning"><?php echo $val->error('title'); ?></p>
+       <?php endif;?>
+       <!-- 
+	    タイトルフォームend
+	 -->
+       
+       <!-- 
+	    コメントフォームstart
+	 -->
+       <div class="form-group">
+	 <?php echo Form::label('レビュー', 'comment'); ?>
+	 <?php if(isset($input_comment)){$input_2 = $input_comment;}else{$input_2 = '';} ?>
+	 <?php echo Form::textarea('comment', $input_2, array('rows'=>10, 'cols'=>30)); ?> 
+       </div>
+       <!-- 
+	    コメントのエラー
+	 -->
+       <?php if(isset($val) && $val->error('comment')): ?>
+       <p class="alert alert-warning"><?php echo $val->error('comment'); ?></p>
+       <?php endif;?>
+       <!-- 
+	    コメントフォームend
+	 -->
+       
+       <!-- 
+	    評価フォームstart
+	 -->
+       <div class="form-group">
+	 <?php echo Form::label('評価', 'rating'); ?>
+	 <?php if(isset($input_rating)){$input_3 = $input_rating;}else{$input_3 = null;} ?>
+	 <?php echo Form::select('rating', $input_3, array( '' => "----", 5.0 => 5.0, 4.0 => 4.0, 3.0 => 3.0, 2.0 => 2.0, 1.0 => 1.0)); ?> 
+       </div>
+       <!-- 
+	    評価のエラー
+	 -->
+       <?php if(isset($val) && $val->error('rating')): ?>
+       <p class="alert alert-warning"><?php echo $val->error('rating'); ?></p>
+       <?php endif;?>
+       <!-- 
+	    評価フォームend
+	 -->
+       <?php echo Form::submit('submit', 'レビューを投稿', array('class'=>'btn btn-default')); ?> 
+       <?php echo Form::close(); ?> <br>
 
   </body>
 </html>
